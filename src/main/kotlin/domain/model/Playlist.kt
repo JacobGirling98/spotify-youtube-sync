c9289d5c3b604832e5b9993a42a13f6713583565
Jacob <jacob.girling98@gmail.com>
2025-12-22 17:19:06 +0000

    playlist delta function

--- src/main/kotlin/domain/model/Playlist.kt
+++ src/main/kotlin/domain/model/Playlist.kt
@@ -1,7 +1,22 @@
 package org.example.domain.model
 
+data class Delta(
+    val added: List<Song> = emptyList(),
+    val removed: List<Song> = emptyList()
+) {
+    companion object {
+        fun empty() = Delta(emptyList(), emptyList())
+    }
+}
+
 data class Playlist(
     val id: Id,
     val name: Name,
     val songs: SongDictionary
-)
+) {
+    fun deltaWith(other: Playlist): Delta {
+        val added = other.songs.entries.keys.filter { song -> song !in songs.entries.keys }
+        val removed = songs.entries.keys.filter { song -> song !in other.songs.entries.keys }
+        return Delta(added, removed)
+    }
+}
---
+++ src/test/kotlin/unit/domain/model/PlaylistTest.kt
@@ -0,0 +1,96 @@
+package unit.domain.model
+
+import io.kotest.matchers.shouldBe
+import org.example.domain.model.*
+import kotlin.test.Test
+
+class PlaylistTest {
+    val songA = Song(Name("Song A"), listOf(Artist("Artist A")))
+    val songB = Song(Name("Song B"), listOf(Artist("Artist B")))
+
+
+    @Test
+    fun `deltaWith returns an empty Delta when the playlists are the same`() {
+        val sourcePlaylist = Playlist(
+            Id("SP1"),
+            Name("My Playlist"),
+            SongDictionary(
+                songA to ServiceIds(Service.SPOTIFY to Id("s1")),
+            )
+        )
+        val targetPlaylist = Playlist(
+            Id("YTP1"),
+            Name("My Playlist"),
+            SongDictionary(
+                songA to ServiceIds(Service.YOUTUBE_MUSIC to Id("yt1")),
+            )
+        )
+
+        sourcePlaylist.deltaWith(targetPlaylist) shouldBe Delta.empty()
+    }
+
+    @Test
+    fun `deltaWith gives songs that are missing in the other playlist`() {
+        val sourcePlaylist = Playlist(
+            Id("SP1"),
+            Name("My Playlist"),
+            SongDictionary(
+                songA to ServiceIds(Service.SPOTIFY to Id("s1")),
+                songB to ServiceIds(Service.SPOTIFY to Id("s2")),
+            )
+        )
+        val targetPlaylist = Playlist(
+            Id("YTP1"),
+            Name("My Playlist"),
+            SongDictionary(
+                songA to ServiceIds(Service.YOUTUBE_MUSIC to Id("yt1")),
+            )
+        )
+
+        sourcePlaylist.deltaWith(targetPlaylist) shouldBe Delta(removed = listOf(songB))
+    }
+
+    @Test
+    fun `deltaWith gives songs that are added in the other playlist`() {
+        val sourcePlaylist = Playlist(
+            Id("SP1"),
+            Name("My Playlist"),
+            SongDictionary(
+                songA to ServiceIds(Service.SPOTIFY to Id("s1")),
+            )
+        )
+        val targetPlaylist = Playlist(
+            Id("YTP1"),
+            Name("My Playlist"),
+            SongDictionary(
+                songA to ServiceIds(Service.YOUTUBE_MUSIC to Id("yt1")),
+                songB to ServiceIds(Service.YOUTUBE_MUSIC to Id("yt2")),
+            )
+        )
+
+        sourcePlaylist.deltaWith(targetPlaylist) shouldBe Delta(added = listOf(songB))
+    }
+    
+    @Test
+    fun `deltaWith handles both added and missing songs in the other playlist`() {
+        val sourcePlaylist = Playlist(
+            Id("SP1"),
+            Name("My Playlist"),
+            SongDictionary(
+                songA to ServiceIds(Service.SPOTIFY to Id("s1")),
+            )
+        )
+        val targetPlaylist = Playlist(
+            Id("YTP1"),
+            Name("My Playlist"),
+            SongDictionary(
+                songB to ServiceIds(Service.YOUTUBE_MUSIC to Id("yt2")),
+            )
+        )
+
+        sourcePlaylist.deltaWith(targetPlaylist) shouldBe Delta(
+            added = listOf(songB),
+            removed = listOf(songA)
+        )
+    }
+}
