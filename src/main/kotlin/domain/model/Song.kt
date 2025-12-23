395bc00def65d95e62ab4950a7e9e83eb3c25dbd
Jacob <jacob.girling98@gmail.com>
2025-12-23 21:04:59 +0000

    moving comparisons to individual types

--- src/main/kotlin/domain/model/Song.kt
+++ src/main/kotlin/domain/model/Song.kt
@@ -4,26 +4,7 @@ data class Song(
     val name: Name,
     val artists: List<Artist>
 ) {
-
     override fun toString(): String {
         return "${name.value} - ${artists.joinToString { it.value }}"
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Song
-
-        if (name.value.lowercase() != other.name.value.lowercase()) return false
-        if (artists != other.artists) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = name.hashCode()
-        result = 31 * result + artists.hashCode()
-        return result
     }
 }
--- src/main/kotlin/domain/model/TinyTypes.kt
+++ src/main/kotlin/domain/model/TinyTypes.kt
@@ -1,7 +1,34 @@
 package org.example.domain.model
 
-@JvmInline value class Name(val value: String)
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonValue
 
-@JvmInline value class Artist(val value: String)
+data class Name(val value: String) {
+    override fun equals(other: Any?): Boolean = other is Name && value.equals(other.value, ignoreCase = true)
+    override fun hashCode(): Int = value.lowercase().hashCode()
+
+    @JsonValue
+    fun toJson(): String = value
+
+    companion object {
+        @JvmStatic
+        @JsonCreator
+        fun fromJson(value: String): Name = Name(value)
+    }
+}
+
+data class Artist(val value: String) {
+    override fun equals(other: Any?): Boolean = other is Artist && value.equals(other.value, ignoreCase = true)
+    override fun hashCode(): Int = value.lowercase().hashCode()
+
+    @JsonValue
+    fun toJson(): String = value
+
+    companion object {
+        @JvmStatic
+        @JsonCreator
+        fun fromJson(value: String): Artist = Artist(value)
+    }
+}
 
 @JvmInline value class Id(val value: String)
